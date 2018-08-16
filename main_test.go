00ad9c7bf90af2a72b3942941b8f62f441ebf0db
J <j.e.aten@gmail.com>
2018-08-16 13:45:18 -0400

    init

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	cv "github.com/glycerine/goconvey/convey"
+	"github.com/glycerine/verb"
+)
+
+var _ = fmt.Printf
+var vv = verb.VV
+var _ = vv
+
+func Test001(t *testing.T) {
+	cv.Convey("Given, when, then", t, func() {
+
+	})
+}
